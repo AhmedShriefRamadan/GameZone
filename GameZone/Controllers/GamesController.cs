0e512783ed49e6d1906271ced870c096ffe253f7
AhmedShriefRamadan <ahmedshrieframadan@gmail.com>
2025-09-13 22:09:54 +0300

    Add Form Inputs

--- GameZone/Controllers/GamesController.cs
+++ GameZone/Controllers/GamesController.cs
@@ -1,9 +1,18 @@
+using GameZone.Data;
+using GameZone.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GameZone.Controllers;
 
 public class GamesController : Controller
 {
+    private readonly AppDbContext _Context;
+
+    public GamesController(AppDbContext context)
+    {
+        _Context = context;
+    }
     public IActionResult Index()
     {
         return View();
@@ -11,6 +20,26 @@ public class GamesController : Controller
 
     public IActionResult Create()
     {
-        return View();
+        CreateGameFormViewModel viewModel = new()
+        {
+            Categories = _Context.Categories
+                                    .Select(c => new SelectListItem
+                                    {
+                                        Value = c.Id.ToString(),
+                                        Text = c.Name
+                                    })
+                                    .OrderBy(c => c.Text)
+                                    .ToList(),
+            Devices = _Context.Devices
+                                .Select(d => new SelectListItem
+                                {
+                                    Value = d.Id.ToString(),
+                                    Text = d.Name
+                                })
+                                .OrderBy(d => d.Text)
+                                .ToList()
+
+        };
+        return View(viewModel);
     }
 }
--- GameZone/ViewModels/CreateGameFormViewModel.cs
+++ GameZone/ViewModels/CreateGameFormViewModel.cs
@@ -6,9 +6,10 @@ public class CreateGameFormViewModel
 {
     [MaxLength(255)]
     public string Name { get; set; } = default!;
+    [Display(Name = "Category")]
     public int CategoryId { get; set; }
     public IEnumerable<SelectListItem> Categories { get; set; } = default!;
-
+    [Display(Name = "Supported Devices")]
     public List<int> SelectedDevices { get; set; } = default!;
     public IEnumerable<SelectListItem> Devices { get; set; } = default!;
     [MaxLength(2555)]
