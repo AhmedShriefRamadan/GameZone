608fd060d0ce50b25773e715adf5d6f99ac5eb35
AhmedShriefRamadan <ahmedshrieframadan@gmail.com>
2025-09-16 10:04:59 +0300

    Install select2 lib using libman

---
+++ GameZone/wwwroot/js/filesize-validation.js
@@ -0,0 +1,5 @@
+// Custom rule: file size
+$.validator.addMethod("filesize", function (value, element, param) {
+    var isValid = this.optional(element) || element.files[0].size <= param;
+    return isValid;
+});
